d69160c234e448517e8521151f7f742583fb30d2
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2021-09-20 15:48:04 +0700

    add new unit test line

--- lib/src/test/java/Line/LineTest.java
+++ lib/src/test/java/Line/LineTest.java
@@ -6,12 +6,25 @@ import org.junit.jupiter.api.Test;
 class LineTest {
 
     @Test
-    public void calculateLength() {
+    public void calculateLengthShouldReturnResult() {
         double answer = 5;
         int x1 = 0;
         int x2 = 3;
         int y1 = 0;
         int y2 = 4;
+
+        Line line = new Line(x1, x2, y1, y2);
+        double distance = line.calculateLength();
+        assertEquals(answer, distance, 0.0001);
+    }
+
+    @Test
+    public void calculateLengthShouldReturnZero() {
+        double answer = 0;
+        int x1 = 0;
+        int x2 = 0;
+        int y1 = 0;
+        int y2 = 0;
 
         Line line = new Line(x1, x2, y1, y2);
         double distance = line.calculateLength();
